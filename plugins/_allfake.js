585287ed44776ff796c2e23a91fc5326712bd079
picolasYT <nicolasguevarakraan@gmail.com>
2026-01-18 15:32:16 -0300

    Arreglando el .play y mejorando el .menu

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -19,11 +19,11 @@ global.mes = d.toLocaleDateString('es', {month: 'long'})
 global.año = d.toLocaleDateString('es', {year: 'numeric'})
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true})
 
-var canal = 'https://whatsapp.com/channel/0029VbAfPu9BqbrEMFWXKE0d'  
-var comunidad = 'https://chat.whatsapp.com/I0dMp2fEle7L6RaWBmwlAa'
-var git = 'https://github.com/The-King-Destroy'
-var github = 'https://github.com/The-King-Destroy/Yuki_Suou-Bot' 
-var correo = 'thekingdestroy507@gmail.com'
+var canal = 'https://whatsapp.com/channel/0029VbBvO6u2f3EGGL1Hx22u'  
+var comunidad = 'https://whatsapp.com/channel/0029VbBvO6u2f3EGGL1Hx22u'
+var git = 'https://github.com/picolasYT'
+var github = 'https://github.com/picolasYT/Ghost-BOT' 
+var correo = 'picolassoporte@gmail.com'
 global.redes = [canal, comunidad, git, github, correo].getRandom()
 
 global.nombre = m.pushName || 'Anónimo'
