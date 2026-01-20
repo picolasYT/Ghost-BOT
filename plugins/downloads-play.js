f4482e02c327baba322b3b1708fa27d260313fec
picolasYT <nicolasguevarakraan@gmail.com>
2026-01-20 15:43:45 -0300

    Intentando arreglar .play

--- plugins/downloads-play.js
+++ plugins/downloads-play.js
@@ -111,8 +111,8 @@ const downloadMedia = async (conn, m, url, type) => {
     const sent = await conn.sendMessage(m.chat, { text: msg }, { quoted: m })
 
     const apiUrl = type === "mp3"
-      ? `https://api-adonix.ultraplus.click/download/ytaudio?url=${encodeURIComponent(url)}&apikey=SHADOWKEYBOTMD`
-      : `https://api-adonix.ultraplus.click/download/ytvideo?url=${encodeURIComponent(url)}&apikey=SHADOWKEYBOTMD`
+      ? `https://gawrgura-api.onrender.com/download/ytmp3?url=${encodeURIComponent(url)}&apikey=SHADOWKEYBOTMD`
+      : `https://gawrgura-api.onrender.com/download/ytmp4?url=${encodeURIComponent(url)}&apikey=SHADOWKEYBOTMD`
 
     const r = await fetch(apiUrl)
     const data = await r.json()
